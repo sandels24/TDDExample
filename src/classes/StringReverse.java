d6600d73e4151b2791050459536b6faf46f22c8b
sandels24 <saiteja.sandela@amwater.com>
2018-11-20 11:18:29 -0500

    Initial Commit

---
+++ src/classes/StringReverse.java
@@ -0,0 +1,33 @@
+package classes;
+
+/**
+ * 
+ * @author SANDELS
+ *
+ */
+public class StringReverse {
+
+	public static void main(String[] args) {
+
+		StringReverse strrev =  new StringReverse();
+		strrev.reverse("hello");
+	}
+	
+	/**
+	 * 
+	 * @param str
+	 * @return 
+	 */
+	public String reverse(String str) {
+		if(str == null)
+			return null;
+		if(str.length()==0 || str.length()==1)
+			return str;
+		
+		StringBuilder rev =  new StringBuilder();
+		for(int i=str.length()-1;i>=0;i--) {
+			rev.append(str.charAt(i));
+		}
+		return rev.toString();
+	}
+}
---
+++ src/test/TestReverse.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import classes.StringReverse;
+
+/**
+ * @author SANDELS
+ *
+ */
+class TestReverse {
+
+	static StringReverse sr;
+	
+	@BeforeAll
+	static void initiate() {
+		sr = new StringReverse();
+	}
+	
+	@Test
+	void testNull1() {
+		assertNull(sr.reverse(null));
+	}
+	@Test
+	void testNull2() {
+		assertEquals("", sr.reverse(""));
+	}
+	@Test
+	void testSingleChar() {
+		assertEquals("h", sr.reverse("h"));
+	}
+	@Test
+	void testTwoChars() {
+		assertEquals("eh", sr.reverse("he"));
+	}
+	@Test
+	void testMoreThanTwo() {
+		assertEquals("olleh", sr.reverse("hello"));
+	}
+}
